dd43b6d607c004c32d144e16dc55a881e298a9f4
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-14 02:58:53 +0530

    We don't want any archives

--- fdroid/config.py
+++ fdroid/config.py
@@ -1,9 +1,11 @@
 repo_url = "https://calyxos.gitlab.io/calyxos-fdroid-repo/fdroid/"
-repo_name = "CalyxOS F-Droid repo (unofficial)"
+repo_name = "CalyxOS F-Droid repo (local)"
 repo_icon = "fdroid-icon.png"
 repo_description = """
 CalyxOS Local F-Droid repo (not so local anymore though!)
 """
+
+archive_older = 0
 
 local_copy_dir = "/fdroid"
 
