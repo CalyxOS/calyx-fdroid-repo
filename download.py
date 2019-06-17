40994f66e8dec70913a9d417170c151d4c43662f
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-18 01:18:39 +0530

    Save downloaded file as app name
    * Needed to keep things simple on the AOSP build side, easier
      to have be named consistently

--- download.py
+++ download.py
@@ -27,17 +27,17 @@ def main():
       ignore = apk["ignoreErrors"]
     if "architectures" in apk:
       for arch in apk["architectures"]:
-        download(apk["baseUrl"].format(arch=arch, ver=ver), ignore)
+        download(apk["name"] + ".apk", apk["baseUrl"].format(arch=arch, ver=ver), ignore)
     else:
-      download(apk["baseUrl"].format(ver=ver), ignore)
+      download(apk["name"] + ".apk", apk["baseUrl"].format(ver=ver), ignore)
   with open('cache/versions.json', 'w') as file:
     json.dump(versions, file, ensure_ascii=False)
 
-def download(download_url, ignore):
+def download(name, download_url, ignore):
   if download_url.endswith(".apk"):
-    retcode = subprocess.call(["wget", "--progress=dot:mega", "-N", "-P", "fdroid/repo", download_url])
+    retcode = subprocess.call(["wget", "--progress=dot:mega", "-N", "-P", "fdroid/repo", "-O", name, download_url])
   else:
-    retcode = subprocess.call(["wget", "--progress=dot:mega", "-nc", "--content-disposition", "-P", "fdroid/repo", download_url])
+    retcode = subprocess.call(["wget", "--progress=dot:mega", "-nc", "--content-disposition", "-P", "fdroid/repo", "-O", name, download_url])
   if not ignore and retcode != 0:
     raise Exception("Failed downloading " + download_url)
 
