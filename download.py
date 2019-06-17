3faf353dfe016abaa0734c8e9206cba0c8a5dc27
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-18 05:15:30 +0530

    Add support for downloading apks from F-Droid repos

--- download.py
+++ download.py
@@ -3,7 +3,11 @@
 import json
 import re
 import requests
+import shutil
 import subprocess
+
+import fdroidserver.common
+import fdroidserver.index
 
 def main():
   with open("apks.json") as file:
@@ -19,6 +23,8 @@ def main():
         ver = get_version_json(verObj["url"], verObj["json"])
       elif "regex" in verObj:
         ver = get_version_regex(verObj["url"], verObj["regex"])
+      elif "fdroid" in verObj:
+        ver = get_version_fdroid(apk["baseUrl"].format(ver=""), verObj["fdroid"])
       if apk["name"] in versions and ver == versions[apk["name"]]:
         continue
       versions[apk["name"]] = ver
@@ -34,10 +40,7 @@ def main():
     json.dump(versions, file, ensure_ascii=False)
 
 def download(name, download_url, ignore):
-  if download_url.endswith(".apk"):
-    retcode = subprocess.call(["wget", "--progress=dot:mega", "-N", "-P", "fdroid/repo", "-O", name, download_url])
-  else:
-    retcode = subprocess.call(["wget", "--progress=dot:mega", "-nc", "--content-disposition", "-P", "fdroid/repo", "-O", name, download_url])
+  retcode = subprocess.call(["wget", "--progress=dot:mega", "-O", "fdroid/repo/" + name, download_url])
   if not ignore and retcode != 0:
     raise Exception("Failed downloading " + download_url)
 
@@ -54,6 +57,19 @@ def get_version_json(url, query):
   for query_part in query:
     version = version[query_part]
   return version
+
+def get_version_fdroid(url, query):
+  fdroidserver.common.config = {}
+  fdroidserver.common.config['jarsigner'] = shutil.which('jarsigner')
+  # TODO Add fingerprint, and use etag
+  data, etag = fdroidserver.index.download_repo_index(url, None, False)
+  for app in data['apps']:
+    if app['packageName'] == query:
+      for apk in data['packages'][app['packageName']]:
+        # No idea why suggestedVersionCode is a string
+        if apk['versionCode'] == int(app['suggestedVersionCode']):
+          return apk['apkName']
+      return data['packages'][app['packageName']][0]['apkName']
 
 if __name__ == "__main__":
   main()
