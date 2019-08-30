451f3e5ca8964602625e52c6eed9935a54a8abf4
Chirayu Desai <chirayudesai1@gmail.com>
2019-08-30 18:46:52 +0530

    download: Fetch missing apk from our main F-Droid repo

--- download.py
+++ download.py
@@ -47,7 +47,7 @@ def main():
       download(apk["name"] + ".apk", apk["baseUrl"].format(ver=ver, ver_stripped=ver.lstrip("v")), ignore)
   for apk in apks:
     if not os.path.isfile("fdroid/repo/" + apk["name"] + ".apk"):
-      download(apk["name"] + ".apk", "https://gitlab.com/calyxos/platform_prebuilts_calyx/raw/pie/fdroid/repo/" + apk["name"] + ".apk" ,ignore)
+      download(apk["name"] + ".apk", "https://fdroid-repo.calyxinstitute.org/fdroid/repo/" + apk["name"] + ".apk" ,ignore)
   with open('cache/versions.json', 'w') as file:
     json.dump(versions, file, ensure_ascii=False)
 
